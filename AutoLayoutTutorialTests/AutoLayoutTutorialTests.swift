54800b9659240e676c798c3343d1c8b3daedd1b0
Amir Magomedov <dimain347@gmail.com>
2024-10-14 19:30:40 +0300

    Initial Commit

---
+++ AutoLayoutTutorialTests/AutoLayoutTutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  AutoLayoutTutorialTests.swift
+//  AutoLayoutTutorialTests
+//
+//  Created by Амир Магомедов on 14.10.2024.
+//
+
+import Testing
+@testable import AutoLayoutTutorial
+
+struct AutoLayoutTutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
